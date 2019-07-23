7ff2260051b0c366d6fe664c6eb404e04eec986d
Chuah Hui Ren <huirenchuah@gmail.com>
2019-07-23 16:53:32 +0930

    aboutus

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -19,6 +19,7 @@ export class MenuComponent implements OnInit {
     const highlight = document.createElement('span');
     document.body.appendChild(highlight);
     highlight.classList.add('highlight');
+    
 
     // Set initial dimensions and position of 'highlight' based on activeButton coords 
     function initialHightlightLocation() {
